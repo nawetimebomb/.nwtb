a5e367b7a9089f296911a144147dda6343d8bfe7
Nahuel Jesús Sacchetti <nahueljsacchetti@gmail.com>
2017-07-10 17:38:30 -0300

    Update configuration to add Zenburn base16

--- profile.sh
+++ profile.sh
@@ -9,12 +9,9 @@
 export NWTB=$HOME/.nwtb
 export NWTB_PACKAGES=${NWTB}/packages
 
-# PACKAGES VARIABLES
-export BASE16_SHELL=${NWTB_PACKAGES}/base16-shell
-
 # ENV VARIABLES
-export EDITOR="/usr/bin/emacs -Q"
-export PS1="\[\e[0;32m\]\w \$ \[\e[m\] "
+export EDITOR="/usr/bin/emacs -Q -nw"
+export PS1="\[\e[0;92m\]\w \$\[\e[m\] "
 export SECRETS="$HOME/.elnawe.secrets/"
 export WWW_HOME="https://www.google.com"
 
@@ -22,7 +19,8 @@ export WWW_HOME="https://www.google.com"
 alias g=git
 
 # LAUNCH PACKAGES
-[ -n "$PS1" ] && [ -s $BASH16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
+BASE16_SHELL=$NWTB_PACKAGES/base16-shell/
+[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
 
 # LAUNCH TMUX WHEN START
 if [ -z "$TMUX" ]; then tmux new-session -A -s main; fi
