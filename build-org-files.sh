98286f18a498a4d46737c54f6b1bc5d8c763a536
Nahuel Jesús Sacchetti <nahueljsacchetti@gmail.com>
2017-07-29 17:24:46 -0300

    Add Tmux configuration with literate programming

--- build-org-files.sh
+++ build-org-files.sh
@@ -1,7 +1,7 @@
 # TODO: Add build by options.
 # TODO: Add all build types
 
-INPUT__CONFIGS_DIR="$HOME/.nwtb/configs"
+INPUT__CONFIGURATIONS_DIR="$HOME/.nwtb/configurations"
 
 runEmacsBabel() {
     FILE=$1
@@ -10,11 +10,18 @@ runEmacsBabel() {
     emacs --batch --eval "(require 'org)" --eval "(org-babel-tangle-file (symbol-name '$FILE) (symbol-name '$OUTPUT))"
 }
 
-# start   --     i3/config.org
-INPUT__I3_CONFIG_FILE="$INPUT__CONFIGS_DIR/i3/config.org"
+# start   --     tmux/configuration.org
+INPUT__TMUX_CONFIG_FILE="$INPUT__CONFIGURATIONS_DIR/tmux/configuration.org"
+OUTPUT__TMUX_CONFIG_FILE="$HOME/.tmux.conf"
+
+runEmacsBabel $INPUT__TMUX_CONFIG_FILE $OUTPUT__TMUX_CONFIG_FILE
+# end     --     ~/.tmux.conf.org
+
+# start   --     i3/configuration.org
+INPUT__I3_CONFIG_FILE="$INPUT__CONFIGURATIONS_DIR/i3/configuration.org"
 OUTPUT__I3_CONFIG_DIR="$HOME/.config/i3"
 OUTPUT__I3_CONFIG_FILE="$OUTPUT__I3_CONFIG_DIR/config"
 
 mkdir -p $OUTPUT__I3_CONFIG_DIR &&
     runEmacsBabel $INPUT__I3_CONFIG_FILE $OUTPUT__I3_CONFIG_FILE
-# end     --     i3/config.org
+# end     --     ~/.config/i3/config
