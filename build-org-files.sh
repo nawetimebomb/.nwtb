87f87ca57e4ea7cf989906911425e5cdd0c257fc
Nahuel Jesús Sacchetti <nahueljsacchetti@gmail.com>
2017-07-29 11:40:26 -0300

    Start with new configurations, literate programming. Add todo.org to keep track of work in progress. Add build-org-files.sh, first version, to build with Emacs and org-babel all the configuration files

---
+++ build-org-files.sh
@@ -0,0 +1,5 @@
+# TODO: Add build by options.
+# TODO: Add all build types
+
+# i3/config.org
+emacs --batch --eval "(require 'org)" --eval '(org-babel-tangle-file "~/.nwtb/i3/config.org" "~/.nwtb/i3/config")'
