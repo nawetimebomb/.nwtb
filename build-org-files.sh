2ebb8664bdc78a6d06f502f5ad376c010f3051c7
Nahuel Jesús Sacchetti <nahueljsacchetti@gmail.com>
2017-07-29 13:12:49 -0300

    Update org-babel to build right into destination folder. Add folder configs

--- build-org-files.sh
+++ build-org-files.sh
@@ -1,5 +1,20 @@
 # TODO: Add build by options.
 # TODO: Add all build types
 
-# i3/config.org
-emacs --batch --eval "(require 'org)" --eval '(org-babel-tangle-file "~/.nwtb/i3/config.org" "~/.nwtb/i3/config")'
+INPUT__CONFIGS_DIR="$HOME/.nwtb/configs"
+
+runEmacsBabel() {
+    FILE=$1
+    OUTPUT=$2
+
+    emacs --batch --eval "(require 'org)" --eval "(org-babel-tangle-file (symbol-name '$FILE) (symbol-name '$OUTPUT))"
+}
+
+# start   --     i3/config.org
+INPUT__I3_CONFIG_FILE="$INPUT__CONFIGS_DIR/i3/config.org"
+OUTPUT__I3_CONFIG_DIR="$HOME/.config/i3"
+OUTPUT__I3_CONFIG_FILE="$OUTPUT__I3_CONFIG_DIR/config"
+
+mkdir -p $OUTPUT__I3_CONFIG_DIR &&
+    runEmacsBabel $INPUT__I3_CONFIG_FILE $OUTPUT__I3_CONFIG_FILE
+# end     --     i3/config.org
