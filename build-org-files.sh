84395a327349b6143c6f36d189a82af9af8b3e79
Nahuel Jesús Sacchetti <nahueljsacchetti@gmail.com>
2017-07-30 02:42:53 -0300

    Add mutt configuration. Move weechat configuration to new folder (WIP)

--- build-org-files.sh
+++ build-org-files.sh
@@ -10,6 +10,14 @@ runEmacsBabel() {
     emacs --batch --eval "(require 'org)" --eval "(org-babel-tangle-file (symbol-name '$FILE) (symbol-name '$OUTPUT))"
 }
 
+# start   --     mutt/configuration.org
+INPUT__MUTT_CONFIG_FILE="$INPUT__CONFIGURATIONS_DIR/mutt/configuration.org"
+OUTPUT__MUTT_CONFIG_FILE="$HOME/.muttrc"
+
+runEmacsBabel $INPUT__MUTT_CONFIG_FILE $OUTPUT__MUTT_CONFIG_FILE
+# end     --     ~/.muttrc
+
+
 # start   --     tmux/configuration.org
 INPUT__TMUX_CONFIG_FILE="$INPUT__CONFIGURATIONS_DIR/tmux/configuration.org"
 OUTPUT__TMUX_CONFIG_FILE="$HOME/.tmux.conf"
