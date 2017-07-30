6c542a5f873aa33a64952350b990a340983255e2
Nahuel Jesús Sacchetti <nahueljsacchetti@gmail.com>
2017-07-30 12:20:36 -0300

    Add neomutt, replace mutt

--- build-org-files.sh
+++ build-org-files.sh
@@ -10,12 +10,12 @@ runEmacsBabel() {
     emacs --batch --eval "(require 'org)" --eval "(org-babel-tangle-file (symbol-name '$FILE) (symbol-name '$OUTPUT))"
 }
 
-# start   --     mutt/configuration.org
-INPUT__MUTT_CONFIG_FILE="$INPUT__CONFIGURATIONS_DIR/mutt/configuration.org"
-OUTPUT__MUTT_CONFIG_FILE="$HOME/.muttrc"
+# start   --     neomutt/configuration.org
+INPUT__MUTT_CONFIG_FILE="$INPUT__CONFIGURATIONS_DIR/neomutt/configuration.org"
+OUTPUT__MUTT_CONFIG_FILE="$HOME/.neomuttrc"
 
 runEmacsBabel $INPUT__MUTT_CONFIG_FILE $OUTPUT__MUTT_CONFIG_FILE
-# end     --     ~/.muttrc
+# end     --     ~/.neomuttrc
 
 
 # start   --     tmux/configuration.org
